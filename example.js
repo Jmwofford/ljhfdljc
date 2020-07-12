3df6391700fe0ebddf66aa4573f67a7048b075b6
Jordan Wofford <jmwofford-codes@gmail.com>
2020-07-12 15:46:26 -0500

    something:

---
+++ example.js
@@ -0,0 +1,17 @@
+1) there are no changes yet
+
+// first initialize repo with  -->  git init
+
+// second grab remote line from git --> git remote add origin https://github.com/Jmwofford/exampleLydRepo.git
+
+// --> i know this works b/c ---> git remote -v
+
+1) <code></code>
+1) <code></code>
+1) <code></code>
+1) <code></code>
+1) <code></code>
+1) <code></code>
+1) <code></code>
+1) <code></code>
+1) <code></code>
