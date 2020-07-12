fe52cc4c86b0239bc63ed6cc5e031f4ed8fb0fa5
Jordan Wofford <jmwofford-codes@gmail.com>
2020-07-12 15:49:16 -0500

    hello

--- example.js
+++ example.js
@@ -15,3 +15,13 @@
 1) <code></code>
 1) <code></code>
 1) <code></code>
+
+// after i made changes 
+// ---->   git add .
+// ---->   git commit -m "whateverRelevantMessageYouCreate"
+// ----> git push origin master 
+
+
+// afterThis ...all will be pushed and merged into git account 
+
+give me something nu to commit
